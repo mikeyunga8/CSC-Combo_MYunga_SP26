9108a26dc2ace7a1d1f42dc60d94cd72e0889504
Yunga <S03121072@ccc.ccofc.edu>
2026-04-21 10:22:55 -0600

    new tictactoe

--- ClassesCheckSheet/src/CSclasses.java
+++ ClassesCheckSheet/src/CSclasses.java
@@ -9,35 +9,59 @@ public class CSclasses {
 		Course course;
 		String another = "yes";
 		while(another.charAt(0) == 'y') {
-		System.out.println("What type of course is it? 1 = In-Person, 2 = Real Time Remote, 3 = Full Online");
-		int type = input.nextInt();
-		System.out.println("What is the course number?");
-		String courseNum = input.next();
-		System.out.println("How many students are already in this class");
-		int numStudents = input.nextInt();
-		System.out.println("What is the maximum number of students");
-		int maxStudents = input.nextInt();
-		System.out.println("How many credits?");
-		int credits = input.nextInt();
-		if(type == 1) {
-			System.out.println("What is the room number");
-			String roomNum = input.next();
-			course = new InPersonCourse(courseNum, numStudents, maxStudents, credits, roomNum);
+			System.out.println("What type of course is it? 1 = In-Person, 2 = Real Time Remote, 3 = Full Online");
+			int type = input.nextInt();
+			System.out.println("What is the course number?");
+			String courseNum = input.next();
+			System.out.println("How many students are already in this class");
+			int numStudents = input.nextInt();
+			System.out.println("What is the maximum number of students");
+			int maxStudents = input.nextInt();
+			System.out.println("How many credits?");
+			int credits = input.nextInt();
+			if(type == 1) {
+				System.out.println("What is the room number");
+				String roomNum = input.next();
+				course = new InPersonCourse(courseNum, numStudents, maxStudents, credits, roomNum);
+			}
+			else if(type == 2) {
+				System.out.println("What is the Zoom Info?");
+				String zoomInfo = input.next();
+				course = new InPersonCourse(courseNum, numStudents, maxStudents, credits, zoomInfo);
+			}
+			else {
+				System.out.println("What is the email");
+				String email = input.next();
+				course = new FullRemoteCourse(courseNum, numStudents, maxStudents, credits, email);
+			}
+			courseList.add(course);
+			System.out.println("Do you need to enter another?");
+			another = input.next();
+			another = another.toLowerCase();
 		}
-		else if(type == 2) {
-			System.out.println("What is the Zoom Info?");
-			String zoomInfo = input.next();
-			course = new InPersonCourse(courseNum, numStudents, maxStudents, credits, zoomInfo);
+		for(Course course1 : courseList) {
+			getPrinter(course1);
 		}
-		else {
-			System.out.println("What is the email");
-			String email = input.next();
-			course = new FullRemoteCourse(courseNum, numStudents, maxStudents, credits, email);
+	}
+	public static void print(Course course) {
+		System.out.println(course);
+	}
+	public static void getPrinter(Course course) {
+		System.out.println("Course: " + course.getCourse());
+		System.out.println("Number of Students: " + course.getNumStudents());
+		System.out.println("Max Students: " + course.getMaxStudents());
+		System.out.println("Credits: " + course.getCredits());
+		if(course instanceof InPersonCourse) {
+			course = (InPersonCourse)course;
+			System.out.println("\n" + course.getRoomNum);
 		}
-		courseList.add(course);
-		System.out.println("Do you need to enter another?");
-		another = input.next();
-		another = another.toLowerCase();
+		else if(course instanceof FullRemoteCourse) {
+			course = (FullRemoteCourse)course;
+			System.out.println("\n" + course.getEmail);
+		}
+		else if(course instanceof RealTimeRemoteCourse) {
+			course =(RealTimeRemoteCourse)course;
+			System.out.println("\n" + course.);
 		}
 	}
 
---
+++ NewTicTacToe/src/tic_tac_toe.java
@@ -0,0 +1,137 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**************************************************
+ * This is a Tic Tac Toe game created with code. * It will need a player
+ * variable and a board * The player variable will be an int that * alternates
+ * between 1 and 2 * * the board will be an array of chars
+ * 
+ * @author S03121072 *
+ * @date 3/4/26 * *
+ **************************************************/
+public class tic_tac_toe {
+
+	public static void main(String[] args) {
+		int player = 2;
+		char[][] board = new char[3][3];
+		fillBoard(board);
+		drawBoard(board);
+		int count = 0;
+		while (count < 9 && checkWinner(board, player) < 1) {
+			player = player % 2 + 1;
+			makeMove(board, player);
+			drawBoard(board);
+
+			count++;
+		}
+		if (checkWinner(board, player) > 0) {
+			System.out.println("Player " + player + " is the winner!!!");
+		} else
+			System.out.println("Cat's Game!!");
+	}
+
+	private static int checkWinner(char[][] board, int player) {
+		String line = null;
+		for (int i = 0; i < 8; i++) {
+			switch (i) {
+			case 0: //top horiz
+				line = String.valueOf(board[0][1]) + board[0][1] + board[0][2];
+				break;
+				
+			case 1: //mid horiz
+				line = String.valueOf(board[1][0]) + board[1][1] + board[1][2];
+				break;
+				
+			case 2: //bottom horiz
+				line = String.valueOf(board[2][0]) + board[2][1] + board[2][2];
+				break;
+				
+			case 3: //left vrt
+				line = String.valueOf(board[0][0]) + board[1][0] + board[2][0];
+				break;
+
+			case 4:	//mid vert
+				line = String.valueOf(board[1][1]) + board[1][2] + board[1][3];
+				break;
+
+			case 5: // right vert
+				line = String.valueOf(board[0][2]) + board[1][2] + board[2][2];
+				break;
+
+			case 6: // 
+				line = String.valueOf(board[0][]) + board[4][] + board[][];
+				break;
+
+			case 7:
+				line = String.valueOf(board[]][]) + board[][] + board[][];
+				break;
+			}
+			if (line.equals("XXX") || line.equals("OOO"))
+				return player;
+
+		}
+		return -1;
+	}
+
+	private static void makeMove(char[][] board, int player) {
+		Scanner input = new Scanner(System.in);
+		int row = -1;
+		int col = -1;
+		char marker = 'X';
+		if (player == 2) {
+			marker = 'O';
+		}
+		while (true) {
+			System.out.println("Player " + player + ", enter a position (1-9): ");
+			if (input.hasNextInt()) {
+				int place = input.nextInt();
+				if (place >= 1 && place <= 9) {
+					// The "Magic" Math
+					row = (place - 1) / 3;
+					col = (place - 1) % 3;
+
+					// Check if the spot is available
+					if (Character.isDigit(board[row][col])) {
+						board[row][col] = marker;
+						break;
+					} else {
+						System.out.println("That spot is already taken!");
+					}
+				} else {
+					System.out.println("Invalid range. Please pick 1-9");
+				}
+			} else {
+				System.out.println("Please enter a number.");
+				input.next(); // clear invalid input
+			}
+		}
+	}
+
+	private static void drawBoard(char[][] board) {
+		for (int i = 0; i < 3; i++) { // rows
+			for (int j = 0; j < 3; j++) { // columns
+				System.out.print(board[i][j]);
+				if (j != 2)
+					System.out.print('|');
+			}
+			System.out.println();
+			if (i != 2)
+				System.out.println("~~~~~");
+		}
+	}
+
+	private static void fillBoard(char[][] board) {
+		int index = 0;
+		char ch = '1';
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				board[i][j] = ch;
+				ch++;
+				index++;
+
+			}
+		}
+
+	}
+
+}
